57b4e07e97b3cf74650a88065b0236ad3a4e1fb0
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-07-12 16:56:47 +0530

    added:lineproblem

--- src/Line.java
+++ src/Line.java
@@ -19,6 +19,33 @@ public class Line {
 
 	 double length=Math.sqrt((c-a)*(c-a)+(d-b)*(d-b));
 	System.out.println("length is= " +length);
-	}
+	
+	
+	System.out.println("enter values for first line ");
+	System.out.println("p1");
+	int p1=sc.nextInt();
+	
+	System.out.println("q1");
+	int q1=sc.nextInt();
+	System.out.println("enter value for second line");
+	System.out.println("p2");
+	int p2=sc.nextInt();
 
+	System.out.println("q2");
+	int q2=sc.nextInt();
+	
+
+	 double length2=Math.sqrt((p2-p1)*(p2-p1)+(q2-q1)*(q2-q1));
+	System.out.println("length 2 is= " +length2);
+	
+	if(length == length2) 
+	{
+		System.out.println("line are equal");
+	}
+	else 
+	{
+		System.out.println("line are not equal");
+	}
+	
+	}
 }
