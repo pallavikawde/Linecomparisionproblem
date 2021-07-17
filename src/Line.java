22b689f71892cb4951f59c629e5f7008f3e951c0
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-07-17 18:37:37 +0530

    Added:oops concept

--- src/Line.java
+++ src/Line.java
@@ -1,70 +1,78 @@
 import java.util.Scanner;
-//create class line
 
+class Linec
+{
+    double lenght;
+    double lenght_2;
+    public void lenght()
+    {
+        Scanner sc = new Scanner(System.in);
+        // taking Input for 1st line
+        System.out.println("Enter value for line_1");
+        System.out.println("X1 is");
+        int x1 = sc.nextInt();
+
+        System.out.println("Y1 is");
+        int y1 = sc.nextInt();
+
+
+        System.out.println("X2 is");
+        int x2 = sc.nextInt();
+
+        System.out.println("Y2 is");
+        int y2 = sc.nextInt();
+        // calculated length
+        lenght = Math.sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1));
+        System.out.println("Lenght of line_1 is "+lenght);
+
+
+        // taking input for 2nd line
+        System.out.println("Enter value for line_2");
+        System.out.println("A1 is");
+        int a1 = sc.nextInt();
+
+        System.out.println("B1 is");
+        int b1 = sc.nextInt();
+
+
+        System.out.println("A2 is");
+        int a2 = sc.nextInt();
+
+        System.out.println("B2 is");
+        int b2 = sc.nextInt();
+        // calculated length
+        lenght_2 = Math.sqrt((a2-a1) * (a2-a1) + (b2-b1) * (b2-b1));
+        System.out.println("Lenght of line_2 is "+lenght_2);
+    }
+
+    public void lineCompare()
+    {
+        if(lenght == lenght_2)
+        {
+            System.out.println("line are equal");
+        }
+        else
+        {
+            System.out.println("line are not equal");
+        }
+
+        if(lenght > lenght_2)
+        {
+            System.out.println("line 1 is greater");
+        }
+        if(lenght < lenght_2)
+        {
+            System.out.println("line 2 is greater");
+        }
+    }
+}
 public class Line {
-	//use main methode
-	
-	public static void main (String[] args) {
-		//take user input first line
-		
-	Scanner sc = new Scanner(System.in);
-	System.out.println("enter values for first line ");
-	System.out.println("x1");
-	int a=sc.nextInt();
-	
-	System.out.println("y1");
-	int b=sc.nextInt();
-	
-	System.out.println("x2");
-	int c=sc.nextInt();
 
-	System.out.println("y2");
-	int d=sc.nextInt();
-	
-//use  length formula &calculate length for first line
-	
-	 double length=Math.sqrt((c-a)*(c-a)+(d-b)*(d-b));
-	System.out.println("length is= " +length);
-	
-	//input for second line
-	
-	System.out.println("enter values for second line ");
-	System.out.println("p1");
-	int p1=sc.nextInt();
-	
-	System.out.println("q1");
-	int q1=sc.nextInt();
+    public static void main(String[] args)
+    {
+        Linec obj = new Linec();
+        obj.lenght();
+        obj.lineCompare();
+    }
 
-	System.out.println("p2");
-	int p2=sc.nextInt();
-
-	System.out.println("q2");
-	int q2=sc.nextInt();
-	
-//calculated length for second line
-	
-	 double length2=Math.sqrt((p2-p1)*(p2-p1)+(q2-q1)*(q2-q1));
-	System.out.println("length 2 is= " +length2);
-	
-	//check length is equal or not
-	
-	if(length == length2) 
-	{
-		System.out.println("line are equal");
-	}
-	else 
-	{
-		System.out.println("line are not equal");
-	}
-	// compare two length
-	
-	if (length>length2 ) {
-	System.out.println("length is greater");
-	}
-	if(length2>length) {
-		System.out.println("length2 is greater");
-	
-	}		
-	}}	
-	
-
+}
